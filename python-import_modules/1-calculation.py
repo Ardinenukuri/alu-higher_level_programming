264ce8be78b1f05fbcf826b6e036abe2aaa70c2d
Ardinenukuri <a.nukuri@alustudent.com>
2022-10-19 05:54:41 -0700

    la 3eme projet

---
+++ python-import_modules/1-calculation.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+from calculator_1.py import add, sub, mul, div
+if __name__ == '__main__':
+a = 10
+b = 5
+def add(a, b)
+    return (a +b)
+print (add())
+def sub(a, b)
+    return (a -b)
+print (sub ())
+def mul(a, b)
+   return (a +b)
+print mul (a *b)
+def div(a, b)
+   return (a /b)
+print div(a, b)
