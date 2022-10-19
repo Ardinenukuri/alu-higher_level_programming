8445cbae982bb71fe050796028765aa75e829f4b
Ardinenukuri <a.nukuri@alustudent.com>
2022-10-19 05:38:52 -0700

    la 3eme projet

--- python-import_modules/0-add.py
+++ python-import_modules/0-add.py
@@ -1,8 +1,7 @@
 #!/usr/bin/python3
 from add_0.py import add
 
-a= 1
-b= 2
-
 if __name__ == '__main__':
-    print("{} + {} = {}".format(a, b, add(a, b)))
+    a = 1
+    b = 2
+    print("{:d} + {:d} = {:d}".format(a, b, add(a, b)))
