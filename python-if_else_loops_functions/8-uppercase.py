46fbbcb3f0c71f14c3c894e6672e5a797796d0ed
Ardinenukuri <a.nukuri@alustudent.com>
2022-10-19 07:55:40 -0700

    lolo

--- python-if_else_loops_functions/8-uppercase.py
+++ python-if_else_loops_functions/8-uppercase.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
-def uppercase(str1):
-    for char in str1:
+def uppercase(str):
+    for char in str:
         if 123 > ord(char) > 96:
             char = chr(ord(char) - 32)
-            print('{}'.format(char),, end="")
+            print('{}'.format(char), end="")
         print('')
