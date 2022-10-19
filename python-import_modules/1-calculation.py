4f275671ac0df3ef6cf0a56832e9f24197edefac
Ardinenukuri <a.nukuri@alustudent.com>
2022-10-19 06:07:27 -0700

    klk modification

--- python-import_modules/1-calculation.py
+++ python-import_modules/1-calculation.py
@@ -1,17 +1,10 @@
 #!/usr/bin/python3
-from calculator_1.py import add, sub, mul, div
-if __name__ == '__main__':
+from calculator_1 import add, sub, mul, div
+
 a = 10
 b = 5
-def add(a, b)
-    return (a +b)
-print (add())
-def sub(a, b)
-    return (a -b)
-print (sub ())
-def mul(a, b)
-   return (a +b)
-print mul (a *b)
-def div(a, b)
-   return (a /b)
-print div(a, b)
+if __name__ == '__main__':
+    print(f"{:d} + {:d} = {:d}".format(a, b, add(a, b)))
+    print("{:d} - {:d} = {:d}".format(a, b, sub(a, b)))
+    print("{:d} * {:d} = {:d}".format(a, b, mul(a, b)))
+    print("{:d} / {:d} = {:d}".format(a, b, div(a, b)))
