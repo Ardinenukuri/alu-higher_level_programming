dfe9ec9ce647b10f91907fc3f7786f09439e4705
Ardinenukuri <109416235+Ardinenukuri@users.noreply.github.com>
2022-11-01 12:21:19 +0200

    Update 6-print_matrix_integer.py

--- python-data_structures/6-print_matrix_integer.py
+++ python-data_structures/6-print_matrix_integer.py
@@ -3,8 +3,8 @@ def print_matrix_integer(matrix=[[]]):
     for list_ in matrix:
         if not list_:
             print()
-        for item in list_:                                          
+        for item in list_:
             if item == list_[-1]:
                 print('{:d}'.format(item))
-            else:       
+            else:
                 print('{:d}'.format(item), end=" ")
