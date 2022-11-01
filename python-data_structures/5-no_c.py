de3bf25876a284a5ab829c9d63b415b9aa58d846
Ardinenukuri <a.nukuri@alustudent.com>
2022-11-01 03:18:40 -0700

    pjct 4

---
+++ python-data_structures/5-no_c.py
@@ -0,0 +1,7 @@
+_c(my_string):
+ = my_string.translate({ord("c"): None})
+
+
+            my_string = my_string.translate({ord("C"): None})
+
+                return my_string
---
+++ python-data_structures/8-multiple_returns.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+def multiple_returns(sentence):
+
+        if sentence:
+
+                    sentence_length = len(sentence)
+
+                        else:
