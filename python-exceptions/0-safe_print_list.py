de99479eb286f554fb6c3040acb91ddf0338e11d
Ardinenukuri <109416235+Ardinenukuri@users.noreply.github.com>
2022-11-08 22:58:13 +0200

    Update 0-safe_print_list.py

--- python-exceptions/0-safe_print_list.py
+++ python-exceptions/0-safe_print_list.py
@@ -1,1 +1,11 @@
-0-safe_print_list.py
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    count = 0
+    for i in range(x):
+        try:
+            print(my_list[i], end="")
+            count += 1
+        except IndexError:
+            pass
+    print()
+    return count
