e8116678c9d996ca3e88c857cb30a063e2fb3b9c
Ardinenukuri <109416235+Ardinenukuri@users.noreply.github.com>
2022-11-14 23:59:56 +0200

    Update 100-my_int.py

--- python-inheritance/100-my_int.py
+++ python-inheritance/100-my_int.py
@@ -1,1 +1,17 @@
-100-my_int.py
+#!/usr/bin/python3
+"""inherits from int"""
+
+
+class MyInt(int):
+    """MyInt class inherits from int"""
+    def __eq__(self, other_int):
+        """inverted =="""
+        if self is not other_int:
+            return False
+        return True
+
+    def __ne__(self, other_int):
+        """inverted !="""
+        if self is other_int:
+            return False
+        return True
