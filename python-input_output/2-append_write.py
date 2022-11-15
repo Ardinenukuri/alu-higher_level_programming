0dd2d82906fc552205cab3e23003b97cff14244a
Ardinenukuri <109416235+Ardinenukuri@users.noreply.github.com>
2022-11-15 12:18:32 +0200

    Update 2-append_write.py

--- python-input_output/2-append_write.py
+++ python-input_output/2-append_write.py
@@ -1,1 +1,11 @@
-2-append_write.p2-append_write.pyy
+#!/usr/bin/python3
+"""
+Module 2-append_write.py
+"""
+
+
+def append_write(filename="", text=""):
+    """writes a string to text file, returns number of char"""
+
+    with open(filename, mode="a", encoding="utf-8") as f:
+        return (f.write(text))
