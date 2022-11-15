c75ee294c462180f6516e80f55362c85cf941fda
Ardinenukuri <109416235+Ardinenukuri@users.noreply.github.com>
2022-11-15 12:19:47 +0200

    Update 4-from_json_string.py

--- python-input_output/4-from_json_string.py
+++ python-input_output/4-from_json_string.py
@@ -1,1 +1,11 @@
-4-from_json_string.p4-from_json_string.pyy
+#!/usr/bin/python3
+"""
+Module 4-from_json_string.py
+"""
+
+
+def from_json_string(my_str):
+    """returns python data structure frm JSON"""
+    import json
+
+    return json.loads(my_str)
