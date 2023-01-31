acb388208e8c8514f45d58fb23687645fdf2c8d7
Ardinenukuri <a.nukuri@alustudent.com>
2023-01-31 12:44:02 +0200

    pupu

--- javascript-warm_up/1-multi_languages.js
+++
@@ -1,5 +0,0 @@
-#!usr/bin/node
-const myVar = "C is fun";
-const myVar = "Python is cool";
-const myVar = "JavaScript is amazing";
-console.log(myVar)
--- javascript-warm_up/3-value_argument.js
+++
@@ -1,7 +0,0 @@
-#!/usr/bin/node
-
-if (process.argv[2] === undefined) {
-    consolr.log('No argument');
-} else {
-    console.log(process.argv[2]);
-}
