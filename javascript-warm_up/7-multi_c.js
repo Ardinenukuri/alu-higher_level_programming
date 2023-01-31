a0e1f24822e6e1d4ea0786e9ab65d8b912b07f22
Ardinenukuri <109416235+Ardinenukuri@users.noreply.github.com>
2023-01-31 12:06:06 +0200

    Update 7-multi_c.js

--- javascript-warm_up/7-multi_c.js
+++ javascript-warm_up/7-multi_c.js
@@ -1,1 +1,11 @@
-7-multi_c.js
+#!/usr/bin/node
+
+const arg = parseInt(process.argv[2], 10);
+
+if (isNaN(arg)) {
+    console.log('Missing number of occurrences');
+} else {
+    for (let i = 1; i <= arg; i++) {
+	console.log('C is fun');
+    }
+}
