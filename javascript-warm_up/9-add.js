a6703bc3201db263004ce40ee3720453366be583
Ardinenukuri <109416235+Ardinenukuri@users.noreply.github.com>
2023-01-31 12:07:06 +0200

    Update 9-add.js

--- javascript-warm_up/9-add.js
+++ javascript-warm_up/9-add.js
@@ -1,1 +1,8 @@
-9-add.js
+#!/usr/bin/node
+
+const a = parseInt(process.argv[2], 10);
+const b = parseInt(process.argv[3], 10);
+function add (a, b) {
+    return (a + b);
+}
+console.log(add(a, b));
