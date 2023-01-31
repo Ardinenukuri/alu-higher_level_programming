c77d1e50d2d6f7872c28a2db5ed7a62f66b37049
Ardinenukuri <109416235+Ardinenukuri@users.noreply.github.com>
2023-01-31 12:05:01 +0200

    Update 5-to_integer.js

--- javascript-warm_up/5-to_integer.js
+++ javascript-warm_up/5-to_integer.js
@@ -1,1 +1,9 @@
-5-to_integer.js
+#!/usr/bin/node
+
+const num = parseInt(process.argv[2], 10);
+
+if (isNaN(num)) {
+    console.log('Not a number');
+} else {
+    console.log(`My number: ${num}`);
+}
